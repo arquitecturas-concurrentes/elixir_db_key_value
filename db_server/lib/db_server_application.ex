1489c19acfa5d0f56b4e428b20b289ade3de0192
Sebastian <scelestino@despegar.com>
2016-07-18 12:26:41 -0300

    fix

--- db_server/lib/db_server_application.ex
+++ db_server/lib/db_server_application.ex
@@ -2,7 +2,7 @@ defmodule DBServerApplication do
 	use Application	
 
 	def start(_type, _args) do
-		DB.Server.Supervisor.start_link(OwesomeDatabase)
+		DB.Server.Supervisor.start_link(AwesomeDatabase)
 	end	
 
 end
